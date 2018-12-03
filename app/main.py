393f714c9cc73ac9d0d52a12d0a44a626a615a4d
indira.bobburi <indira.bobburi@servicenow.com>
2018-12-03 13:39:41 -0800

    Integration frontend and middleware

--- app/main.py
+++ app/main.py
@@ -12,28 +12,14 @@ CORS(app)
 def first_page():
     return render_template("index.html")
     
-@app.route('/test', methods=['POST'])
-def test():
+@app.route('/predict', methods=['POST'])
+def predict():
      data = request.get_json()
-     print("coming here")
      print(data)
      resp = make_response(json.dumps(data))
      resp.status_code = 200
      resp.headers['Access-Control-Allow-Origin'] = '*'
      return resp
-
-# @app.route('/getPrediction', methods=['POST', 'GET'])
-# def getPrediction():
-# 	if request.method == 'POST':
-# 		print("Reaching here")
-# 	return render_template("index.html", result="Slight")
-
-@app.route('/prediction', methods=['POST','GET'])
-def prediction():
-    if request.method == 'POST':
-#         asd = request.json
-        print("coming in post")
-    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, threaded=True)
