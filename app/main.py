2bb8c5764264d7545c1f0048a1daa0307f2c4960
indira.bobburi <indira.bobburi@servicenow.com>
2018-12-03 13:27:20 -0800

    WIP: Integration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
-from flask import request, redirect
+from flask import request, redirect, make_response
 from flask_cors import CORS
 import os
 import os.path
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +11,16 @@ CORS(app)
 @app.route("/")
 def first_page():
     return render_template("index.html")
+    
+@app.route('/test', methods=['POST'])
+def test():
+     data = request.get_json()
+     print("coming here")
+     print(data)
+     resp = make_response(json.dumps(data))
+     resp.status_code = 200
+     resp.headers['Access-Control-Allow-Origin'] = '*'
+     return resp
 
 # @app.route('/getPrediction', methods=['POST', 'GET'])
 # def getPrediction():
@@ -17,11 +28,11 @@ def first_page():
 # 		print("Reaching here")
 # 	return render_template("index.html", result="Slight")
 
-@app.route('/prediction', methods=['POST'])
+@app.route('/prediction', methods=['POST','GET'])
 def prediction():
     if request.method == 'POST':
-        asd = request.json
-        print(asd)
+#         asd = request.json
+        print("coming in post")
     return render_template("index.html")
 
 if __name__ == "__main__":
