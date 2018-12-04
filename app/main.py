4102d1c96575423939df7ecd6e51e328068cd212
Indira Priyadarshini Bobburi <31742324+IndiraBobburi@users.noreply.github.com>
2018-12-03 20:44:23 -0800

    Additionally UI changes

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask import request, redirect, make_response
+# from flask.ext.responses import json_response
 from flask_cors import CORS
 import os
 import os.path
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = "", static_folder = "tmp")
 CORS(app)
 
 @app.route("/")
@@ -16,10 +17,14 @@ def first_page():
 def predict():
      data = request.get_json()
      print(data)
-     resp = make_response(json.dumps(data))
+     r = {"result":"Slight"}
+     # result = "Slight"
+     resp = make_response(json.dumps(r))
      resp.status_code = 200
      resp.headers['Access-Control-Allow-Origin'] = '*'
      return resp
+     
+     #return json_response(r, status_code=201)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, threaded=True)
